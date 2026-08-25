74734e608d6462b36b3c8836a9b15da3ceed35ae
sai02-creator <164014757+sai02-creator@users.noreply.github.com>
2026-08-26 00:49:32 +1000

    Add demo property type definition

---
+++ src/types/demo-property.ts
@@ -0,0 +1,11 @@
+export type DemoProperty = {
+    id: string;
+    title: string;
+    image: string;
+    city: string;
+    pricePerNight: number;
+    hostName: string;
+    rating: number;
+    maxGuests: number;
+    availableDates: string[];
+};
